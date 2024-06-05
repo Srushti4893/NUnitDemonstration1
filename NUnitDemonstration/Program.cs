0d56812f0a83dba9f359da125e7f5907bec36e26
Srushti4893 <spatel3017@conestogac.on.ca>
2024-06-05 16:31:53 -0400

    Add project files.

---
+++ NUnitDemonstration/Program.cs
@@ -0,0 +1,38 @@
+﻿using NUnitDemonstration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstAngle = 60;
+        int secondAngle = 60;
+        int thirdAngle = 60;
+        string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+        Console.WriteLine(result);
+    }
+}
---
+++ TestClass/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+using NUnit.Framework.Legacy;
+
+
+namespace TestClass
+{
+    public class TestClass
+    {
+        [TestFixture]
+        public class TestClass1
+        {
+
+
+            [Test]
+
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                int firstAngle = 80;
+                int secondAngle = 90;
+                int thirdAngle = 90;
+               
+                string expected = "The triangle is valid.";
+               
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                ClassicAssert.AreEqual(expected,actual);
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
